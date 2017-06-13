2556919140f3e03cff1812ca0b6b1149a96aba7e
AnilNalawade <anil.nalawade@atos.net>
2017-06-13 08:40:46 +0200

    Add files via upload

--- dbconnection.php
+++ dbconnection.php
@@ -12,6 +12,7 @@ $sql="create table test(id int,name varchar(20))";
 if(mysqli_query($conn,$sql))
 {
  echo "Database test success";
+ echo"<p>Click here for <a href="register.php">Registration</a></p>";
 }
 else
 {
