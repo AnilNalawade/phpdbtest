eb2db298bbd1bb139dfd7c1faf847e03e4577d9c
AnilNalawade <anil.nalawade@atos.net>
2017-06-13 11:28:58 +0200

    Update config.php

--- config.php
+++ config.php
@@ -1,5 +1,5 @@
 <?php
-   define('DB_SERVER','10.129.128.163');
+   define('DB_SERVER','10.131.23.70');
    define('DB_USERNAME','AnilNalawade');
    define('DB_PASSWORD','Nilnil123'); 
    define('DB_DATABASE','sampledb');
